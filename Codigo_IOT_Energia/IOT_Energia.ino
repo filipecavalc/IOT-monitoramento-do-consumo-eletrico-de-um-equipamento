f0179b7a2a1ed2282348819157fd856dba042f31
Filipe S. Cavalcante <filipecavalc@gmail.com>
2017-10-23 17:42:06 -0200

    adicionado a versão final do codigo

--- Codigo_IOT_Energia/IOT_Energia.ino
+++ Codigo_IOT_Energia/IOT_Energia.ino
@@ -1,40 +1,34 @@
 // Importação das Bibliotecas
-#include <Wire.h>
-#include <ESP8266WiFi.h> // Importa a Biblioteca ESP8266WiFi
-#include <PubSubClient.h> // Importa a Biblioteca PubSubClient
-#include <NTPClient.h>
-#include <WiFiUdp.h>
-#include <Time.h>
-#include <TimeLib.h>
-
-#define TOPICO_PUBLISH "consumo_Eletrico_LabIoT"
-
-#define ID_MQTT  "id_consumo_Eletrico_LabIoT_001"
-
-// Portas dos entrada do Sensor
-#define pinoSensor_ACS712_5A A0
-
-// Definições de conexão na rede
-const char* SSID = ""; // SSID nome da rede WI-FI que deseja se conectar
-const char* PASSWORD = ""; // Senha da rede WI-FI que deseja se conectar
+#include <ESP8266WiFi.h>  // Importa a Biblioteca ESP8266WiFi
+#include <PubSubClient.h> // Importa a Biblioteca PubSubClient do MQTT
+#include <NTPClient.h>    // Importa a Biblioteca NTPClient para conexão com servidor NTP para coletar o horario em formato Epoch
+#include <WiFiUdp.h>      // Importa a Biblioteca WiFiUdp para permitir enviar e receber pacotes via UDP
+#include <Time.h>         // Complemento da Biblioteca TimeLib.h
+#include <TimeLib.h>      // Importa Biblioteca para realizar a conversão do formato Epoch para ISO no metodo epochToLabIoTFormat
 
 // MQTT
-const char* BROKER_MQTT = "www.unioeste-foz.br"; //URL ou IP do broker MQTT que se deseja utilizar
-int BROKER_PORT = 1883; // Porta do Broker MQTT
-
+#define TOPICO_PUBLISH "consumo_Eletrico_LabIoT"
+#define ID_MQTT  "id_consumo_Eletrico_LabIoT_001"
+const char* BROKER_MQTT = ""; //URL ou IP do broker MQTT que se deseja utilizar
+int BROKER_PORT = 1883; // Porta do Broker MQTT normalmente a 1883
 WiFiClient espClient; // Cria o objeto espClient
 PubSubClient MQTT(espClient); // Instancia o Cliente MQTT passando o objeto espClient
 
-// NTP get Time - Configurações para consulta do horario atual via internet
-WiFiUDP ntpUDP;
+// Definições de conexão na rede
+const char* SSID = "LabIoT"; // SSID nome da rede WI-FI que deseja se conectar
+const char* PASSWORD = ""; // Senha da rede WI-FI que deseja se conectar
+
+// Configurações para coletar Horario via internet
+WiFiUDP ntpUDP;//biblioteca para envio de mensagem via protocolo UDP
 int16_t utc = -3; //UTC -3:00 Brazil
-NTPClient timeClient(ntpUDP, "a.st1.ntp.br", utc * 3600, 60000);
+NTPClient timeClient(ntpUDP, "a.st1.ntp.br", utc * 3600, 60000);// NTP get Time - Configurações para consulta do horario atual via internet
 
 // Constantes Globais para calculo do consumo
 float mediaCorrente = 0;
-int tensaoRedeEletrica = 127;
-float S_aparente = 0;
-String horarioConvertido = "";
+int tensaoRedeEletrica = 127; // 127 ou ou 220 dependendo da tensão da tomada aonde for ser realizado a medição
+
+// Portas dos entrada do Sensor
+#define pinoSensor_ACS712_5A A0
 
 // Constantes Globais utilizadas no loop
 String mensagem = "";
@@ -44,19 +38,17 @@ void setup() {
   //Incia a Serial
   Serial.begin(9600);
   pinMode(pinoSensor_ACS712_5A, INPUT);
-  initWiFi();
-  initMQTT();
+  reconectWiFi(); // conecta-se a rede WIFI
+  initMQTT(); // conecta-se ao broker MQTT
 }
 
 void loop() {
-
+  
   mensagem.remove(0); //remove todo o conteudo da string a partir da posição passada por parametro (da esquerda para a direita)
-  //getSensor pega a media já calculada do consumo no intervalo de um segundo entre os picos de onda
-  //timeClient.getEpochTime() pega o horario em formato epoch na internet para adicionar na mensagem
-  //epochToLabIoTFormat() faz a conversão do formato epoch para o formato ISO utilizado no banco de dados do servidor MQTT LabIoT
-  mensagem.concat(getSensor());
+  mensagem.concat(getSensor());//getSensor pega a media já calculada do consumo no intervalo de um segundo entre os picos de onda
   mensagem.concat(' ');
-  mensagem.concat(epochToLabIoTFormat(timeClient.getEpochTime()));
+  mensagem.concat(epochToISO(timeClient.getEpochTime()));//timeClient.getEpochTime() pega o horario em formato epoch na internet para adicionar na mensagem
+                                                         //epochToISO() faz a conversão do formato epoch para o formato ISO utilizado no banco de dados do servidor MQTT LabIoT
   Serial.println(mensagem.c_str());
   verificaConexoesWifiAndMQTT();
   enviaEstadoOutputMQTT();
@@ -64,11 +56,11 @@ void loop() {
 }
 
 /*
-   Função: Inicializa Wi-Fi e conecta-se na rede WI-FI desejada
+   Função: Converter horario em formato epoch para ISO
    Parâmetros: horario em formato epoch unsigned int
    Retorno: retorna uma String com o horario epoch convertido para formato ISO
 */
-String epochToLabIoTFormat(unsigned int horario){
+String epochToISO(unsigned int horario){
   
   String mensagem = "";
   if(year(horario) < 10){
@@ -103,15 +95,6 @@ String epochToLabIoTFormat(unsigned int horario){
 
   return mensagem;
  }
- 
-/*
-   Função: Inicializa Wi-Fi e conecta-se na rede WI-FI desejada
-   Parâmetros: nenhum
-   Retorno: nenhum
-*/
-void initWiFi() {
-  reconectWiFi();
-}
 
 /*
    Função: Inicializa MQTT e informa qual broker e porta deve ser conectado
@@ -147,11 +130,17 @@ void reconectWiFi() {
   }
   // Caso contrário, conecta na rede WiFi
   else {
+    while(WiFi.status() == WL_IDLE_STATUS){
+      
+      WiFi.begin(SSID, PASSWORD);
+      
+      uint32_t start_time = millis();
+      while((millis() - start_time) < 1200){} // Espera 1 segundo antes de tentar novamente se conectar ao wifi
+                                              // Utilizando a millis para ter uma melhor precisão
+    }
 
-    WiFi.begin(SSID, PASSWORD);
-
-    timeClient.begin();
-    timeClient.update();
+    timeClient.begin(); // sincroniza relogio
+    timeClient.update(); // atualiza relogio
   }
 
 }
@@ -177,7 +166,7 @@ void reconnectMQTT() {
 }
 
 /*
-   Função: Envia ao Broker o estado atual do output
+   Função: Envia ao Broker a mensagem corrente + data em formato ISO
    Parâmetros: nenhum
    Retorno: nenhum
 */
@@ -192,7 +181,11 @@ void enviaEstadoOutputMQTT(void) {
 
 }
 
-
+/*
+   Função: Ler dados do sensor e realizar calculo da media da corrente através dos picos da corrente no intervalo de 1 segundo
+   Parâmetros: nenhum
+   Retorno: Valor médio do pico de correntes em um intervalo de 1 segundo
+*/
 float getSensor() {
   
   /* Inicia variáveis locais */
@@ -200,15 +193,16 @@ float getSensor() {
   int leituraValor;
   float valorSensor = 0;
   float valorCorrente = 0;
-  float voltsporUnidade = 0.00322581;// 5%1023
-  // Para ACS712 de  5 Amperes use 0.185
-  // Para ACS712 de 10 Amperes use 0.100
-  // Para ACS712 de 30 Amperes use 0.066
-  float sensibilidade = 0.185;
+  float voltsporUnidade = 0.00322581;// 3.3%1023
+
+  float sensibilidade = 0.185;// Para ACS712 de  5 Amperes use 0.185
+                              // Para ACS712 de 10 Amperes use 0.100
+                              // Para ACS712 de 30 Amperes use 0.066
 
   int valorMin = 1024;
   int valorMax = 0;
   uint32_t start_time = millis();
+  //objetivo desse loop é fazer a leituras dos picos em um intervalo de 1 segundo e realizar o calculo baseado no menor e no maior pico
   while ((millis() - start_time) < 1200) // Amostra de 1 segundo
   {
     // Lê o status atual do sensor
